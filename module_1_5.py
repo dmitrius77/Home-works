140d8b9a6ba19dcb5867351fe60c34617ae6b848
dmitrius77 <77dmitrius7777@gmail.com>
2024-11-07 20:57:53 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var=(15, 'Varibable', True)
+print (immutable_var)
+print(type (immutable_var))
+print(immutable_var[1])
+#immutable_var[1]=100 #Объект «кортеж» не поддерживает назначение элементов
+immutable_var=(15, ['Variable', True], 17)
+print(immutable_var)
+mutable_var=(99, ['Module', False])
+mutable_var[1] [1]= True
+print(mutable_var)
